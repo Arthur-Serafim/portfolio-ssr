30d09ab47e681a8dbcb4b645f2969b59b9e1f3ad
Arthur-Serafim <dev.seraph.arthur@gmail.com>
2020-12-07 09:21:23 -0300

    Reduce description even more

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Home: React.FC = () => {
         <title>Arthur Guimarães Serafim | Portfolio</title>
         <meta
           name="description"
-          content="rthur Serafim is an experienced Front End Developer and Software Engineer with a detailed history of collaborating with
+          content="Arthur is an experienced Front End Developer and Software Engineer with a detailed history of collaborating with
             stakeholders and driving exceptional results."
         />
         <link rel="canonical" href="https://arthurserafim.com" />

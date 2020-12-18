0d81345e9e3d1fcd18def34271550a9aa260c8c2
Arthur-Serafim <dev.seraph.arthur@gmail.com>
2020-12-18 11:19:07 -0300

    Update resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,9 +33,8 @@ export default function Navbar({ fullpageApi }) {
           Contact
         </NavigationLabel>
         <a
-          href="https://drive.google.com/u/1/uc?id=19Ra4I0YQELRKNcanM6B8nDSq2qbqtq6B&export=download"
-          target="_blank"
-          rel="norel"
+          href="/assets/Arthur Resume.pdf"
+          download
         >
           <NavigationLabel>Resume</NavigationLabel>
         </a>
